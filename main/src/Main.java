7c9763a167ec1b16b11fe7776d0e915f20b6e31f
gfloriano11 <gflorianodev@gmail.com>
2025-09-05 23:17:35 -0300

    feat: add menu to user select option

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Main{
 
     public static void main(String[] args){
 
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Bem-vindo ao Sistema Gerenciador de Usuários");
+        System.out.println("O que você deseja fazer?");
+        System.out.println("[1] - Inserir Usuário(s)");
+        System.out.println("[2] - Editar Usuário(s)");
+        System.out.println("[3] - Visualizar Usuário(s)");
+        System.out.println("[4] - Deletar Usuário(s)");
+
+        String option = scanner.next();
+        System.out.println("Você escolheu a opção " + option);
     }
 }
