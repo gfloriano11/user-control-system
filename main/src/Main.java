36e9595ef833233e6f0ffc3c6bdb1cbb7b35aa78
gfloriano11 <gflorianodev@gmail.com>
2025-09-06 16:24:51 -0300

    feat: add more options

--- main/src/Main.java
+++ main/src/Main.java
@@ -10,8 +10,10 @@ public class Main{
         System.out.println("O que você deseja fazer?");
         System.out.println("[1] - Inserir Usuário(s)");
         System.out.println("[2] - Editar Usuário(s)");
-        System.out.println("[3] - Visualizar Usuário(s)");
+        System.out.println("[3] - Listar Usuário(s)");
         System.out.println("[4] - Deletar Usuário(s)");
+        System.out.println("[5] - Buscar Usuário");
+        System.out.println("[6] - Sair");
 
         String option = scanner.next();
         System.out.println("Você escolheu a opção " + option);
