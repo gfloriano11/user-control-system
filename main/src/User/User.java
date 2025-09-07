22c1c8d3b9ad8f2db47d45084b30a02e14de3b38
gfloriano11 <gflorianodev@gmail.com>
2025-09-07 20:47:06 -0300

    feat: add role in user atributes

--- main/src/User/User.java
+++ main/src/User/User.java
@@ -1,14 +1,18 @@
+package User;
+
 public class User {
 
     private int id;
     public String name;
     private String email;
     private String password;
+    public boolean role;
 
-    public User(int userId, String userName, String userEmail, String userPass){
+    public User(int userId, String userName, String userEmail, String userPass, boolean userRole){
         this.id = userId;
         this.name = userName;
         this.email = userEmail;
         this.password = userPass;
+        this.role = userRole;
     }
 }
