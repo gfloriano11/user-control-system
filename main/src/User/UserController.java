3da7f7fe56f5963bfcfece9683bc038ec7950934
gfloriano11 <gflorianodev@gmail.com>
2025-09-28 10:40:16 -0300

    feat: add search by user email

--- main/src/User/UserController.java
+++ main/src/User/UserController.java
@@ -54,7 +54,7 @@ public class UserController {
 
         for(User user : users){
             if(user != null){
-                if(Objects.equals(searchTerm, String.valueOf(user.getId())) || Objects.equals(searchTerm.toLowerCase(), (String.valueOf(user.name)).toLowerCase())){
+                if(Objects.equals(searchTerm, String.valueOf(user.getId())) || Objects.equals(searchTerm.toLowerCase(), (String.valueOf(user.name)).toLowerCase()) || Objects.equals(searchTerm.toLowerCase(), (String.valueOf(user.email)).toLowerCase())){
                     searchedUsers.add(user);
                 }
             }
